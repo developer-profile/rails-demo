df544e47b9a2c8e1a8f63d79f2bd9e01753f9575
Dmitri E. Molokov <lleess@yandex.ru>
2012-03-20 12:06:30 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaisinstallerDemo::Application.config.secret_token = 'b84656098fce10bd32f48b0cc713ef08d192c37bd58451ef8059a2ece512a89a85fbbe107b8011cc3674f77903653e073e439f827ae2ce2bdf4dec0c64b9a3b2'
